7a103566d87296ffd8dcc704e1a0aa8e365609f0
Michał Kazula <biuro@cadbit.pl>
2026-01-02 14:45:16 +0100

    added domain Resource - part 3

---
+++ backend/symfony/migrations/Version20260102134234.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260102134234 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE resource (id UUID NOT NULL, type VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, status VARCHAR(255) NOT NULL, unavailability VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN resource.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN resource.type IS \'(DC2Type:resource_type)\'');
+        $this->addSql('COMMENT ON COLUMN resource.status IS \'(DC2Type:resource_status)\'');
+        $this->addSql('COMMENT ON COLUMN resource.unavailability IS \'(DC2Type:resource_unavailability)\'');
+        $this->addSql('COMMENT ON COLUMN resource.created_at IS \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE resource');
+    }
+}
